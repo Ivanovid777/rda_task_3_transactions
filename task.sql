0bc60ee6e01846bfd04ceb86cc5dce72d5fa871c
Ivan <ivanqqq@ukr.net>
2026-03-11 17:13:33 +0100

    trx is created

--- task.sql
+++ task.sql
@@ -2,10 +2,12 @@
 USE ShopDB; 
 
 -- Some data should be created outside the transaction (here)
+INSERT INTO Order(CustomerID, Date) VALUE (1, "2023-01-01");
+-- Start the transaction
+START TRANSACTION;
 
--- Start the transaction 
-START TRANSACTION; 
-
--- And some data should be created inside the transaction 
-
+-- And some data should be created inside the transaction
+INSERT INTO OrderItems(OrderID, ProductID, Count) VALUE (1, 1, 0);
+UPDATE Products SET WarehouseAmount = WarehouseAmount - 1 WHERE ID = 1 and Name = 'AwersomeProduct';
+UPDATE OrderItems SET Count = Count + 1 WHERE OrderID = 1 and ProductID = 1;
 COMMIT; 
